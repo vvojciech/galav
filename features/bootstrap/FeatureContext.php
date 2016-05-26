680a18f0ce2c44e384c04bddc39149f20a5692a1
Wojciech Kucha <github@kucha.pl>
2016-05-26 20:39:19 +0000

    .env.behat update + Feature Context basics filled

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,14 +2,15 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Laracasts\Behat\Context\Migrator;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements Context, SnippetAcceptingContext
 {
+    use Migrator;
+
     /**
      * Initializes context.
      *
@@ -20,4 +21,47 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements 
     public function __construct()
     {
     }
+
+    /**
+     * @Then I should see list of images
+     */
+    public function iShouldSeeListOfImages()
+    {
+        $this->assertElementOnPage(".gallery-item");
+
+    }
+
+    /**
+     * @When I search for :arg1
+     */
+    public function iSearchFor($arg1)
+    {
+        $this->fillField('.search-query', $arg1);
+        $this->pressButton('.search');
+    }
+
+    /**
+     * @Then I should see results of :arg1
+     */
+    public function iShouldSeeResultsOf($arg1)
+    {
+        $this->assertElementContainsText("h1", $arg1);
+    }
+
+    /**
+     * @When I click on any available image
+     */
+    public function iClickOnAnyAvailableImage()
+    {
+        $this->clickLink('.gallery-item');
+    }
+
+    /**
+     * @Then I should see gallery page
+     */
+    public function iShouldSeeGalleryPage()
+    {
+        $this->assertElementOnPage('.single-gallery');
+    }
+
 }
