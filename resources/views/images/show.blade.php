009db4fa78db09f0c54b48b43c7c6258f41cc879
Wojciech Kucha <github@kucha.pl>
2016-06-26 15:31:47 +0000

    adding panel to single view

--- resources/views/images/show.blade.php
+++ resources/views/images/show.blade.php
@@ -11,41 +11,40 @@
         <div class="row">
             <div class="col-md-8 col-sm-8 col-xs-12">
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <img class="img-responsive" src="{{ url('/i/' . $image->filename . '.jpg') }}"/>
-                    </div>
+                <div class="">
+                    <img class="img-responsive" src="{{ url('/i/' . $image->filename . '.jpg') }}"/>
                 </div>
-                <div class="row">
-                    <div class="col-xs-10 col-sm-6">
-                        @include ('images._favourite')
-                        @include ('images._votes')
-                    </div>
-                    <div class="col-sm-4 hidden-xs">
-                        @include ('images._tags')
-                    </div>
-                    <div class="col-xs-2">
-                        @include ('images._report')
-                    </div>
+                <div class="panel">
+                    <div class="panel-body">
 
-                </div>
-                <div class="row">
-                    <ul class="nav nav-tabs nav-justified">
-                        <li role="presentation" class="active"><a data-toggle="tab" href="#comments">Comments</a></li>
-                        <li role="presentation"><a data-toggle="tab" href="#related">Related</a></li>
-                    </ul>
-
-
-                    <div class="tab-content">
-                        <div id="comments" class="tab-pane fade in active">
-                            @include ('images._comments')
+                        <div class="col-xs-10 col-sm-6">
+                            @include ('images._favourite')
+                            @include ('images._votes')
                         </div>
-                        <div id="related" class="tab-pane fade in">
-                            <p>@todo related</p>
+                        <div class="col-sm-4 hidden-xs">
+                            @include ('images._tags')
                         </div>
-                    </div>
+                        <div class="col-xs-2">
+                            @include ('images._report')
+                        </div>
 
+                    </div>
                 </div>
+                <ul class="nav nav-tabs nav-justified">
+                    <li role="presentation" class="active"><a data-toggle="tab" href="#comments">Comments</a></li>
+                    <li role="presentation"><a data-toggle="tab" href="#related">Related</a></li>
+                </ul>
+
+
+                <div class="tab-content">
+                    <div id="comments" class="tab-pane fade in active">
+                        @include ('images._comments')
+                    </div>
+                    <div id="related" class="tab-pane fade in">
+                        <p>@todo related</p>
+                    </div>
+                </div>
+
 
             </div>
             <div class="col-md-4 col-sm-4 hidden-xs">
